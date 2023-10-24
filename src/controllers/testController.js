3da20e2bdee6844b090e279210571fe7393d43de
irpatrick <patricki@andrew.cmu.edu>
2023-10-24 09:57:45 +0200

    Postgress & prisma connection

--- src/controllers/testController.js
+++ src/controllers/testController.js
@@ -1,8 +1,19 @@
 
+const { PrismaClient } = require('@prisma/client')
 
 
-const testGetWay=(req, res)=>{
-    return res.status(200).send({"message":"test"})
+
+const testGetWay= async(req, res)=>{
+    try{
+        const prisma = new PrismaClient()
+
+        // const user = await prisma.user.create({data:{email:"test@test.test", password:"123"}})
+        const users = await prisma.user.findMany({})
+        // console.log(user)
+        return res.status(200).send({"users":users})
+    }catch(e){
+        return res.status(200).send({"error":e})
+    }
 } 
 
 
